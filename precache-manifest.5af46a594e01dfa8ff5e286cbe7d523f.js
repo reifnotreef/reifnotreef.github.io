770d0cf8fbded03e74e080a975ef7b97c7b98354
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2020-01-31 03:01:22 -0600

    Updates

---
+++ precache-manifest.5af46a594e01dfa8ff5e286cbe7d523f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6973d585a177771252c66dbbe4949ad0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a78d2552b5e88b2f757",
+    "url": "/static/css/main.60c23a81.chunk.css"
+  },
+  {
+    "revision": "2b5ef2297154be8f3f4f",
+    "url": "/static/js/2.b49aa175.chunk.js"
+  },
+  {
+    "revision": "0a78d2552b5e88b2f757",
+    "url": "/static/js/main.9a35f1e1.chunk.js"
+  },
+  {
+    "revision": "b59219ec66ff27344c19",
+    "url": "/static/js/runtime-main.0260fe69.js"
+  }
+]);

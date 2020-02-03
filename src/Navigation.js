5ecf415292878a1686baa92bc31e483c88543fba
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2020-02-03 11:21:25 -0600

    added linkedIn to nav

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,6 +2,7 @@
 import HomeIcon from '@material-ui/icons/Home';
 import EmailIcon from '@material-ui/icons/Email';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 import React from 'react';
 
@@ -42,6 +43,13 @@ export const Navigation = () => {
           rel='noopener noreferrer'
         >
           <GitHubIcon htmlColor='white' />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/bridgerhammond/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <LinkedInIcon htmlColor='white' />
         </a>
       </div>
     </div>

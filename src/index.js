178bbdc0d35df97f3ed0a474218ec6f781cce97c
Bridger Reif Hammond <bridgerrhammond@gmail.com>
2020-05-08 15:22:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class App extends Component {
         <Header />
         <ProjectsContainer className="projects-container" />
         <Navigation />
+        <iframe src="https://titanembeds.com/embed/705590384687972453" height="600" width="800" frameborder="0"></iframe>
       </>
     );
   }
